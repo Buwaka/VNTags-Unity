7a06c61964643dd5fd195693ffbb1b89ec6671f5
SYSERVER\SYServer <sammywyd@gmail.com>
2025-09-09 17:41:00 +0200

    Cleanup and proper asmdef

--- Packages/Eflatun.SceneReference-4.1.1/Runtime/Logger.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Eflatun.SceneReference
-{
-    internal static class Logger
-    {
-        internal static void Debug(string msg) => UnityEngine.Debug.Log($"{Constants.LogPrefixBase} {msg}");
-        internal static void Warn(string msg) => UnityEngine.Debug.LogWarning($"{Constants.LogPrefixBase} {msg}");
-        internal static void Error(string msg) => UnityEngine.Debug.LogError($"{Constants.LogPrefixBase} {msg}");
-    }
-}
